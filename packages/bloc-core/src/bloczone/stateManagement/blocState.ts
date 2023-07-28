786831f887e2564afc9f937903bfbebcaf02f76e
Oliver Arthur <OliverArthur@users.noreply.github.com>
2023-07-28 13:45:09 +0200

    make use of the SuscribeManager in the state

--- packages/bloc-core/src/bloczone/stateManagement/blocState.ts
+++ packages/bloc-core/src/bloczone/stateManagement/blocState.ts
@@ -1,4 +1,5 @@
 import type { Subscription } from '../../types'
+import { SubscribeManager } from '../subscription'
 
 /**
  * Create a bloc state with the given initial state.
@@ -15,62 +16,21 @@ export default function createBlocState<S extends object>(initialState: S) {
     throw new Error(`Bloc state must be an object, got ${typeof initialState}`)
   }
 
-  /** @type {WeakMap<object, Subscription<S>>} Mapping of object keys to listener functions */
-  const _subscriberCallbacks = new WeakMap<object, Subscription<S>>()
+  const activeEffect: Subscription<S> | null = null
 
-  /** @type {Set<object>} Set of objects that are subscribed to this Bloc */
-  const _listenerKeys = new Set<object>()
-
-  /** @type {WeakMap<object, Set<keyof S>>} Mapping of object keys to the properties they are subscribed to */
-  const _dependencies = new WeakMap<object, Set<keyof S>>()
-
-  let activeEffect: Subscription<S> | null = null
-
-  const effect = (eff: () => void) => {
-    activeEffect = eff
-    eff()
-    activeEffect = null
-  }
-
-  const trackListeners = (target: object, key: keyof S) => {
-    if (!target || typeof target !== 'object') {
-      console.error(
-        `Track listeners called with invalid target: ${target}, target must be an object`
-      )
-    }
-
-    if (activeEffect) {
-      let deps = _dependencies.get(target)
-      if (!deps) {
-        _dependencies.set(target, (deps = new Set()))
-      }
-      deps.add(key)
-    }
-  }
-
-  const notifyListeners = (target: object, key: keyof S) => {
-    _listenerKeys.forEach((obj) => {
-      const deps = _dependencies.get(obj)
-      if (deps && deps.has(key)) {
-        const listener = _subscriberCallbacks.get(obj)
-        if (listener) {
-          effect(() => listener(target as S))
-        }
-      }
-    })
-  }
+  const manager = SubscribeManager(() => activeEffect)
 
   return new Proxy(initialState, {
-    get: (target, key) => {
+    get: (target, key: string) => {
       const value = Reflect.get(target, key)
-      trackListeners(target, key as keyof S)
+      manager.trackListeners(target, key)
       return value
     },
-    set: (target, key, value) => {
+    set: (target, key: string, value) => {
       const oldValue = Reflect.get(target, key)
       Reflect.set(target, key, value)
       if (oldValue !== value) {
-        notifyListeners(target, key as keyof S)
+        manager.notifyListeners(target, key)
       }
       return true
     }

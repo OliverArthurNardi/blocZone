c567d90785b110daa45c7898b032ef63ede05d43
Oliver Arthur <OliverArthur@users.noreply.github.com>
2023-07-28 13:36:19 +0200

    created subscribeManager

---
+++ packages/bloc-core/src/bloczone/subscription/__tests__/subscribeManager.spec.ts
@@ -0,0 +1,69 @@
+import { describe, test, expect, jest, beforeEach } from '@jest/globals'
+
+import { SubscribeManager } from '../subscribeManager'
+import type { Subscription } from '../../../types'
+
+describe('SubscribeManager', () => {
+  let manager: ReturnType<typeof SubscribeManager>
+  let getActiveEffect: jest.MockedFunction<() => Subscription<object> | null>
+
+  beforeEach(() => {
+    getActiveEffect = jest.fn();
+    manager = SubscribeManager(getActiveEffect)
+    jest.spyOn(console, 'warn').mockImplementation(() => {
+      /* no-op */
+    })
+  })
+
+  test('should subscribe a callback to a target and key', () => {
+    const target = { count: 0 }
+    const callback = jest.fn()
+
+    manager.subscribe(target, 'count', callback)
+
+    manager.notifyListeners(target, 'count')
+
+    expect(callback).toHaveBeenCalledWith(target)
+
+  })
+
+  test('should unsubscribe a callback from a target and key', () => {
+    const target = { }
+    const key = 'value'
+    const callback = jest.fn()
+
+    manager.subscribe(target, key, callback)
+    manager.unsubscribe(target, key)
+
+    manager.notifyListeners(target, key)
+
+    expect(callback).not.toHaveBeenCalled()
+
+  })
+
+  test('shuld throw an error when trying to subscribe to a non-object', () => {
+    const target = 0
+    const callback = jest.fn()
+
+    // @ts-expect-error - Testing invalid input
+    expect(() => manager.subscribe(target, 'count', callback)).toThrowError()
+  })
+
+  test('should throw an error when trying to unsubscribe from a non-object', () => {
+    const target = 0
+    const key = 'count'
+
+    // @ts-expect-error - Testing invalid input
+    expect(() => manager.unsubscribe(target, key)).toThrowError()
+  })
+
+  test('should warn when trying to subscribe an object is already subscribed', () => {
+    const target = { count: 0 }
+    const callback = jest.fn()
+
+    manager.subscribe(target, 'count', callback)
+    manager.subscribe(target, 'count', callback)
+
+    expect(console.warn).toHaveBeenCalled()
+  })
+})
---
+++ packages/bloc-core/src/bloczone/subscription/subscribeManager.ts
@@ -0,0 +1,117 @@
+import type { Subscription } from '../../types'
+
+/**
+ * Create subscription manager with the given active effect.
+ *
+ * @template S - The type of the state.
+ *
+ * @param getActiveEffect - A function that returns the current active effect.
+ *
+ * @returns The created subscription manager.
+ */
+export function SubscribeManager<S extends object>(getActiveEffect: () => Subscription<S> | null) {
+  const _subscriberCallbacks = new WeakMap<object, Subscription<S>>()
+  const _listenerKeys = new Set<object>()
+  const _dependencies = new WeakMap<object, Set<keyof S>>()
+
+  /**
+   * Checks if target is a valid object and is already subscribed, throws an error if not.
+   *
+   * @param target - The target object.
+   */
+  const handleSubscriptionExceptions = (target: object) => {
+    if (typeof target !== 'object' || target === null) {
+      throw new Error(`Cannot subscribe to a non-object value. Received value of type ${typeof target}`)
+    }
+
+    if (_dependencies.has(target)) {
+      console.warn(`Object ${target} is already subscribed. Skipping subscription.`)
+    }
+  }
+
+  /**
+   * Subscribes a listener to changes in a key of the target object.
+   *
+   * @param target - The target object.
+   * @param key - The key in the target object.
+   * @param callback - The callback function.
+   */
+  const subscribe = (target: object, key: string, callback: Subscription<S>) => {
+    handleSubscriptionExceptions(target)
+
+    if (!_dependencies.has(target)) {
+      _dependencies.set(target, new Set())
+    }
+
+    _dependencies.get(target)!.add(key as keyof S)
+    _subscriberCallbacks.set(target, callback)
+    _listenerKeys.add(target)
+  }
+
+  /**
+   * Unsubscribes a listener from changes in a key of the target object.
+   *
+   * @param target - The target object.
+   * @param key - The key in the target object.
+   */
+  const unsubscribe = (target: object, key: string) => {
+    if (typeof target !== 'object' || target === null || !_subscriberCallbacks.has(target)) {
+      throw new Error(`Cannot unsubscribe from a non-subscribed object. Received value of type ${typeof target}`)
+    }
+
+    const dependencies = _dependencies.get(target)!
+    dependencies.delete(key as keyof S)
+
+    if (dependencies.size === 0) {
+      _dependencies.delete(target)
+      _subscriberCallbacks.delete(target)
+      _listenerKeys.delete(target)
+    }
+  }
+
+  /**
+   * Tracks the dependencies of the active effect on the keys of the target object.
+   *
+   * @param target - The target object.
+   * @param key - The key in the target object.
+   */
+  const trackListeners = (target: object, key: string) => {
+    if (!target || typeof target !== 'object') {
+      console.error(`Track listeners called with invalid target: ${target}, target must be an object`)
+    }
+
+    const activeEffect = getActiveEffect()
+    if (activeEffect) {
+      let deps = _dependencies.get(target)
+      if (!deps) {
+        _dependencies.set(target, (deps = new Set()))
+      }
+      deps.add(key as keyof S)
+    }
+  }
+
+  /**
+   * Notifies all listeners that are subscribed to changes in a key of the target object.
+   *
+   * @param target - The target object.
+   * @param key - The key in the target object.
+   */
+  const notifyListeners = (target: object, key: string) => {
+    _listenerKeys.forEach((obj) => {
+      const deps = _dependencies.get(obj)
+      if (deps && deps.has(key as keyof S)) {
+        const listener = _subscriberCallbacks.get(obj)
+        if (listener) {
+          listener(target as S)
+        }
+      }
+    })
+  }
+
+  return {
+    subscribe,
+    unsubscribe,
+    trackListeners,
+    notifyListeners,
+  }
+}
